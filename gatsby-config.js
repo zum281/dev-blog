71e91c0bad57e38638512c9f8cb390fedbfc8839
Fulvia <32518183+zum281@users.noreply.github.com>
2022-02-19 12:13:28 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "1AV46rcG07x196Sl6iLl6nYgXkVjUj4cTBtihpAOBLU",
+      "spaceId": "hsgcuhzq5wje"
+    }
+  }, "gatsby-plugin-emotion", "gatsby-plugin-image", {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": ""
+    }
+  }, "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
