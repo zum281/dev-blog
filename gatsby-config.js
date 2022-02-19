1f99fde5a766db2fd4a1d3744c43bb7b87a42267
Fulvia <32518183+zum281@users.noreply.github.com>
2022-02-19 12:17:20 +0100

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,29 +1,34 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "1AV46rcG07x196Sl6iLl6nYgXkVjUj4cTBtihpAOBLU",
-      "spaceId": "hsgcuhzq5wje"
-    }
-  }, "gatsby-plugin-emotion", "gatsby-plugin-image", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": ""
-    }
-  }, "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        siteUrl: `https://www.yourdomain.tld`,
     },
-    __key: "images"
-  }]
+    plugins: [
+        {
+            resolve: "gatsby-source-contentful",
+            options: {
+                accessToken: "1AV46rcG07x196Sl6iLl6nYgXkVjUj4cTBtihpAOBLU",
+                spaceId: "hsgcuhzq5wje",
+            },
+        },
+        "gatsby-plugin-emotion",
+        "gatsby-plugin-image",
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sitemap",
+        {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                icon: "src/images/icon.png",
+            },
+        },
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/",
+            },
+            __key: "images",
+        },
+    ],
 };
